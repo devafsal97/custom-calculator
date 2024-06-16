caa05268649ba69918f72449610137ffd46ee487
Arun <arun@au-ki.com>
2024-06-17 01:42:24 +0530

    changes

---
+++ public/embed.js
@@ -0,0 +1,10 @@
+(function() {
+  const script = document.createElement('script');
+  script.src = "https://aruns2008.github.io/calculator/static/js/main.3b640362.js";
+  script.onload = function() {
+    const rootDiv = document.createElement('div');
+    rootDiv.id = 'calculator-root';
+    document.body.appendChild(rootDiv);
+  };
+  document.head.appendChild(script);
+})();
